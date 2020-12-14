960f695d5b1e182aa461fa5ac5ec650512597019
Nontas <pantzop@gmail.com>
2020-12-14 19:41:58 +0200

    Added text with full list of cities and keeping only greek cities.
    Signed-off-by: Nontas <pantzop@gmail.com>

--- src/main/java/com/nontas/weatherapp/configuration/GeneralConfig.java
+++ src/main/java/com/nontas/weatherapp/configuration/GeneralConfig.java
@@ -1,11 +1,20 @@
 package com.nontas.weatherapp.configuration;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nontas.weatherapp.model.City;
 import com.nontas.weatherapp.model.OpenWeather;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class GeneralConfig {
@@ -14,4 +23,31 @@ public class GeneralConfig {
         return new OpenWeather();
     }
 
+    @Bean
+    public HashMap<Integer,City> listOfGreekCities(){
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("city.list.json").getFile());
+            HashMap<Integer,City> greekCities = new HashMap<>();
+            ObjectMapper mapper = new ObjectMapper();
+            List<City> cities = new ArrayList<>();
+            JsonFactory factory = new JsonFactory();
+            JsonParser parser = factory.createParser(file);
+            TypeReference<List<City>> tRef = new TypeReference<List<City>>() {};
+            cities = mapper.readValue(parser, tRef);
+
+            for(City city : cities){
+                if(city.getCountry().equals("GR")){
+                    greekCities.put(city.getId(), city);
+                }
+            }
+            return greekCities;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/nontas/weatherapp/controller/WebController.java
+++ src/main/java/com/nontas/weatherapp/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.nontas.weatherapp.controller;
 
+import com.nontas.weatherapp.model.City;
 import com.nontas.weatherapp.model.OpenWeather;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,11 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.HashMap;
+
 @Controller
 public class WebController {
 
     @Autowired
     private OpenWeather openWeatherAndros;
+
+    @Autowired
+    private HashMap<Integer, City> listOfGreekCities;
 
     @GetMapping("/weather")
     public String weather(Model model){
@@ -25,6 +31,12 @@ public class WebController {
 //        System.out.println(openWeatherAndros.toString());
         model.addAttribute("openWeather",openWeatherAndros);
         return "/andros";
+    }
+
+    @GetMapping("/listofcities")
+    public String listOfCities(Model model){
+        model.addAttribute("list",listOfGreekCities);
+        return "/listofcities";
     }
 
 }
